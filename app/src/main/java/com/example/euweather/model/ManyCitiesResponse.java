c474d54ff046bc72236f320d43786e8f799a3bd2
boxsmi <Git@345EDC>
2019-04-18 11:30:41 +0300

    Innitial

---
+++ app/src/main/java/com/example/euweather/model/ManyCitiesResponse.java
@@ -0,0 +1,20 @@
+package com.example.euweather.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class ManyCitiesResponse {
+
+    private int cnt;
+    @SerializedName("list")
+    private List<WeatherInfo> weatherInfoList;
+
+    public int getCnt() {
+        return cnt;
+    }
+
+    public List<WeatherInfo> getWeatherInfoList() {
+        return weatherInfoList;
+    }
+}
